306610dbe4b3d71aa864d4cec6be9db742393072
Sergei Isaenko <isaenko.sv@gmail.com>
2024-04-13 10:58:00 +0300

    Add files via upload

---
+++ src/log_duration.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string_view>
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+
+/**
+ * ������ �������� �����, ��������� � ������� ������ ������
+ * �� ����� �������� �����, � ������� � ����� std::cerr.
+ *
+ * ������ �������������:
+ *
+ *  void Task1() {
+ *      LOG_DURATION("Task 1"sv); // ������� � cerr ����� ������ ������� Task1
+ *      ...
+ *  }
+ *
+ *  void Task2() {
+ *      LOG_DURATION("Task 2"sv); // ������� � cerr ����� ������ ������� Task2
+ *      ...
+ *  }
+ *
+ *  int main() {
+ *      LOG_DURATION("main"sv);  // ������� � cerr ����� ������ ������� main
+ *      Task1();
+ *      Task2();
+ *  }
+ */
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+
+ /**
+  * ��������� ���������� ������� LOG_DURATION, ��� ���� ����� ������� �����,
+  * � ������� ������ ���� �������� ���������� �����.
+  *
+  * ������ �������������:
+  *
+  *  int main() {
+  *      // ������� ����� ������ main � ����� std::cout
+  *      LOG_DURATION("main"s, std::cout);
+  *      ...
+  *  }
+  */
+#define LOG_DURATION_STREAM(x, y) LogDuration UNIQUE_VAR_NAME_PROFILE(x, y)
+
+class LogDuration {
+public:
+    // ������� ��� ���� std::chrono::steady_clock
+    // � ������� using ��� ��������
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(std::string_view id, std::ostream& dst_stream = std::cerr)
+        : id_(id)
+        , dst_stream_(dst_stream) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        dst_stream_ << id_ << ": "sv << duration_cast<milliseconds>(dur).count() << " ms"sv << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const Clock::time_point start_time_ = Clock::now();
+    std::ostream& dst_stream_;
+};
--- src/source.cpp
+++ src/source.cpp
@@ -1,23 +1,80 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <random>
+#include <algorithm>
 
 #include "BST.h"
+#include "AVL.h"
+#include "log_duration.h"
+
+static std::random_device rd; // random device engine, usually based on /dev/random on UNIX-like systems
+// initialize Mersennes' twister using rd to generate the seed
+static std::mt19937 rng{rd()}; 
+
+int dice(int n)
+{
+    static std::uniform_int_distribution<int> uid(1,n); // random dice
+    return uid(rng); // use rng as a generator
+}
 
 int main() {
+    LOG_DURATION("main");
+
+    int n = 1e5;
+
     BST t;
-    for (int i = 1; i <= 4; ++i) {
-        t.Add(i);
+    {
+        LOG_DURATION("BST creation");
+        std::cout << "Common binary tree\n";
+        for (int i = 1; i <= n; ++i) {
+            t.Add(i);
+        }
     }
 
-    t.Print();
+    // AVL
+    AVL<int> avl;
+    std::cout << "AVL tree:\n";
+    {
+        LOG_DURATION("AVL creation");
+        for (int i = 1; i <= n; ++i) {
+            avl.insert(i);
+        }
+    }
 
-    bool b;
-    b = t.Find(6);
-    b = t.Find(26);
+    std::vector<int> v;
+    std::cout << "vector:\n";
+    {
+        LOG_DURATION("Vector creation");
+        v.reserve(n);
+        for (int i = 1; i <= n; ++i) {
+            v.push_back(i);
+        }
+    }
 
-    std::vector<int> v = t.GetAllElements();
-    for (int i : v) std::cout << i << " ";
-    std::cout << std::endl;
+    int m = 1e5;
+
+    {
+        LOG_DURATION("BST search");
+        for (int i = 0; i < m; ++i){
+            t.Find(dice(n));
+        }
+    }
+
+    {
+        LOG_DURATION("AVL search");
+        for (int i = 0; i < m; ++i){
+            avl.Find(dice(n));
+        }
+    }
+
+    {
+        LOG_DURATION("Vector search");
+        for (int i = 0; i < m; ++i){
+            std::vector<int>::iterator It = std::find(v.begin(), v.end(), dice(n));
+        }
+    }
+
+
     return 0;
 }
